11fdde89fdc8638136327611b8debaa3e244e3b2
teripanda <teri.london@gmail.com>
2021-06-28 16:56:34 -0700

    initial terminal and front page setup

---
+++ script.js
@@ -0,0 +1,19 @@
+function sanityCheck() {
+	console.log("i am sane");
+}
+
+sanityCheck();
+
+
+
+
+/* <--- Typewriter Effect ---> */
+
+terminalArray = [
+	"welcome", 
+	"my name is teri london",
+	"i am a software engineer,", 
+	"ux|ui design dev,", 
+	"educator && consultant.", 
+	"click to find out more..."
+];
